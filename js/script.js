9af80b46ed81e8322a3e9c979941e22fb969248f
Peter <iw.kolosov2016@yandex.ru>
2022-05-06 09:17:17 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* let money = prompt('Ваш бюджет на месяц?');
+/* let money = +prompt('Ваш бюджет на месяц?');
 let time = prompt('Введите дату в формате YYYY-MM-DD');
 
 let appData = {
@@ -14,8 +14,18 @@ let appData = {
     savings: false
 }; */
 
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money, time;
+
+function start() {
+	money = +prompt("Ваш бюджет на месяц?", '');
 	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+	while (isNaN(money) || money == '' || money == null) {
+		money = +prompt("Ваш бюджет на месяц?", '');
+	}
+}
+
+start();
 
 let appData = {
 	budget: money,
@@ -23,16 +33,92 @@ let appData = {
 	optionalExpenses: {},
 	income: [],
 	timeData: time,
-	savings: false
+	savings: true
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+/* let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
 	a2 = prompt("Во сколько обойдется?", ''),
 	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	a4 = prompt("Во сколько обойдется?", '');
+	a4 = prompt("Во сколько обойдется?", ''); */
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+/* appData.expenses.a1 = a2;
+appData.expenses.a3 = a4; */
 
-alert(appData.budget / 30);
-/* остановился на п. 3 */
+/* for (let i = 0; i < 2; i++) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+		b = prompt("Во сколько обойдется?", '');
+	
+	if ((typeof a) === 'string' && (typeof a) != null && (typeof b) != null &&
+		a != '' && b != '' && a.length < 50) {
+			console.log('Done!');
+		appData.expenses[a] = b;
+	}
+} */
+
+/* let i = 0;
+while (i < 2) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+		b = prompt("Во сколько обойдется?", '');
+	
+	if ((typeof a) === 'string' && (typeof a) != null && (typeof b) != null &&
+		a != '' && b != '' && a.length < 50) {
+			console.log('Done!');
+		appData.expenses[a] = b;
+	}
+
+	i++;
+} */
+
+/* let i = 0;
+do {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+		b = prompt("Во сколько обойдется?", '');
+	
+	if ((typeof a) === 'string' && (typeof a) != null && (typeof b) != null &&
+		a != '' && b != '' && a.length < 50) {
+			console.log('Done!');
+		appData.expenses[a] = b;
+	}
+	
+	i++;
+} while (i < 2); */
+
+function chooseExpenses() {
+	for (let i = 0; i < 2; i++) {
+		let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+			b = prompt("Во сколько обойдется?", '');
+		
+		if ((typeof a) === 'string' && (typeof a) != null && (typeof b) != null &&
+			a != '' && b != '' && a.length < 50) {
+				console.log('Done!');
+			appData.expenses[a] = b;
+		}
+	}
+}
+
+chooseExpenses();
+
+appData.moneyPerDay = (appData.budget / 30).toFixed();
+
+alert(`Ежедневный бюджет: ${appData.moneyPerDay}`);
+
+if(appData.moneyPerDay < 100) {
+	console.log('Минимальный уровень достатка');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+	console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay > 2000) {
+	console.log('Высокий уровень достатка');
+} else {
+	console.log('Произошла ошибка');
+}
+
+function checkSavings() {
+	if (appData.savings == true) {
+		let save = +prompt('Какова сумма накоплений?', ''),
+			percent = +prompt('Под какой процент?');
+
+		appData.monthIncome = save/100/12*percent;
+		alert(`Доход в месяц с вашего деппозита: ${appData.monthIncome}`);
+	}
+}
+checkSavings();
---
+++ preview/js.js
@@ -0,0 +1,101 @@
+/* let num = 10;
+
+function showFirstMessage(text) {
+    alert(text);
+    let num = 20;
+    console.log(num);
+}
+
+showFirstMessage('Hello World!');
+
+console.log(num); */
+
+/* function calc(a, b) {
+    return (a + b);
+} */
+
+/* let calc = function(a, b) {
+    return (a + b);
+}; */
+
+let calc = (a, b) => a + b;
+
+console.log(calc(3, 4));
+
+console.log(calc(8, 4));
+
+/* console.log(calc(3, 4));
+
+console.log(calc(8, 4)); */
+
+calc(3, 4);
+
+calc(8, 4);
+
+function retVar() {
+    let num = 50;
+    return num;
+}
+
+let anotherNum = retVar();
+
+console.log(anotherNum);
+
+let str = 'test';
+
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.toLocaleUpperCase());
+
+/* let twelve = '12.2'; */
+let twelve = '12.2px';
+
+/* console.log(Math.round(twelve));
+console.log(Math.fround(twelve)); */
+
+console.log(parseInt(twelve));
+console.log(parseFloat(twelve));
+
+
+let randomNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(Math.random(randomNumber));
+
+function first() {
+    /* Что-то делаем */
+    setTimeout(() => {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`Я учу ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('Я прошёл 3й урок!');
+}
+
+/* learnJS('JavaScript', function() {
+    console.log('Я прошёл 3й урок!');
+}); */
+
+learnJS('JavaScript', done);
+
+/* let obj = new Object(); */
+
+let options = {
+    width: 1024,
+    height: 1024,
+    name: 'test'
+};
